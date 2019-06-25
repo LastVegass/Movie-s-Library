6f7c699592d8e8844c2c880a0774050b2f318f57
LastVegass <49659010+LastVegass@users.noreply.github.com>
2019-06-25 20:42:26 +0300

    Add files via upload

--- ruby.rb
+++ ruby.rb
@@ -1,7 +1,7 @@
-def parse_txt_file(file_name)
-file_name.nil? ? 'movies.txt' : file_name
-  if file_name == 'movies.txt'
-    File.read(file_name)
+def parse_txt_file(filename)
+mass = filename.nil? ? 'movies.txt' : filename
+  if mass == 'movies.txt'
+      File.read(mass)
         .split("\n")
         .map { |a| a.split('|') }
         .map { |m| create_movie(m) }
@@ -11,31 +11,30 @@ file_name.nil? ? 'movies.txt' : file_name
 end
 
 def create_movie(movies)
-	{
-	  link: movies[0],
-      name: movies[1],
-      year: movies[2],
-      country: movies[3],
-      realise: movies[4],
-      geners: movies[5],
-      runtime: movies[6],
-      rate: rating_to_stars(movies[7].to_f),
-      director: movies[8],
-      stars: movies[9]
-	}
+  {
+    link: movies[0],
+    name: movies[1],
+    year: movies[2],
+    country: movies[3],
+    realise: movies[4],
+    geners: movies[5],
+    runtime: movies[6],
+    rate: rating_to_stars(movies[7].to_f),
+    director: movies[8],
+    stars: movies[9]
+  }
 end
 
 def movie_with_max_in_name(movies)
   movies.select { |m| m[:name].include? 'Max' }
 end
 
-def movie_with_USA_date(movies)
-  movies.select { |m| m[:country].include? 'USA' }
-  movies.select { |m| m[:year].include? '1995' }
+def movie_with_usa_date(movies)
+  movies.select { |m| m[:year].include?('1995') && m[:country].include?('USA') }
 end
 
 def rating_to_stars(rating)
   '*' * ((rating - 8.0) * 10.0).round
 end
 
-puts movie_with_USA_date(parse_txt_file(ARGV.first))
+puts movie_with_usa_date(parse_txt_file(ARGV.first))
