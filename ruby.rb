d9850b449ee4c7d0c237f526a25a3542bb0ff631
Last_Vegas <AC-130a@yandex.ru>
2019-07-04 21:47:09 +0300

    fix issues
    - fix style & methods

--- ruby.rb
+++ ruby.rb
@@ -16,7 +16,7 @@ def create_movie(movies)
     name: movies[1],
     year: movies[2],
     country: movies[3],
-    realise: movies[4],
+    realise: movies[4].to_i,
     geners: movies[5],
     runtime: movies[6],
     rate: rating_to_stars(movies[7].to_f),
@@ -38,32 +38,32 @@ def rating_to_stars(rating)
 end
 
 def movie_size(movies)
-  movies.map { |m| m[:name].length }
-        .sort.reverse
-        .select { |m| m[:name].length > rightsize[5] }
+  size = movies.map { |m| m[:name].length }
+  rightsize = size.sort.reverse
+  movies.select { |m| m[:name].length > rightsize[5] }
 end
 
-def movie_comedy(movies)
-  movies.select { |m| m[:geners].include?('Comedy') }
-        .map { |m| m[:realise].to_i }
-        .sort
-        .select { |m| m[:realise].to_i < rightdate[10] && m[:geners].include?('Comedy') }
+def movies_comedy(movies)
+  comedylist = movies.select { |m| m[:geners].include?('Comedy') }
+  date = comedylist.map { |m| m[:realise] }
+  rightdate = date.sort
+  movies.select { |m| m[:realise] < rightdate[10] && m[:geners].include?('Comedy') }
 end
 
 def movie_directors_list(movies)
   movies.map { |m| m[:director] }
-        .sort
-        .uniq
         .map { |m| m.split(' ') }
         .map(&:last)
+        .sort
+        .uniq
 end
 
 def movies_not_usa(movies)
   movies.select do |m|
     unless m[:country].include?('USA')
-      puts m[:name], m[:realise], m[:geners], m[:runtime], m[:stars].chomp!
+      puts "#{m[:name]} (#{m[:realise]} ; #{m[:geners]}) - #{m[:runtime]} "
     end
   end
 end
 
-puts movies_not_usa(parse_txt_file(ARGV.first))
+puts movie_size(parse_txt_file(ARGV.first))
