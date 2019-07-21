bb41695e0f6aa8bc457354dfaf4e1e04384e2174
Last_Vegas <AC-130a@yandex.ru>
2019-07-21 16:02:12 +0300

    task_04

--- ruby.rb
+++ ruby.rb
@@ -1,28 +1,28 @@
+require 'csv'
+require 'ostruct'
+
 def parse_txt_file(filename)
   mass = filename.nil? ? 'movies.txt' : filename
   if mass == 'movies.txt'
-    File.read(mass)
-        .split("\n")
-        .map { |a| a.split('|') }
-        .map { |m| create_movie(m) }
+    CSV.read(mass, col_sep: '|')
+       .map { |m| create_movie(m) }
   else
     puts 'incorrect'
   end
 end
 
 def create_movie(movies)
-  {
-    link: movies[0],
-    name: movies[1],
-    year: movies[2],
-    country: movies[3],
-    realise: movies[4],
-    geners: movies[5],
-    runtime: movies[6],
-    rate: rating_to_stars(movies[7].to_f),
-    director: movies[8],
-    stars: movies[9]
-  }
+  OpenStruct.new(:link => movies[0],
+    :name => movies[1],
+    :year => movies[2],
+    :country => movies[3],
+    :realise => movies[4],
+    :geners => movies[5],
+    :runtime => movies[6],
+    :rate => rating_to_stars(movies[7].to_f),
+    :director => movies[8],
+    :stars => movies[9]
+  )
 end
 
 def movie_with_max_in_name(movies)
@@ -38,32 +38,29 @@ def rating_to_stars(rating)
 end
 
 def movie_size(movies)
-  movies.map { |m| m[:name].length }
-        .sort.reverse
-        .select { |m| m[:name].length > rightsize[5] }
+  movies.map { |m| m[:name] }
+        .sort_by(&:length).reverse
+        .take(5)
 end
 
 def movie_comedy(movies)
   movies.select { |m| m[:geners].include?('Comedy') }
-        .map { |m| m[:realise].to_i }
-        .sort
-        .select { |m| m[:realise].to_i < rightdate[10] && m[:geners].include?('Comedy') }
+        .sort_by { |m| m[:realise] }
+        .take(10)
 end
 
 def movie_directors_list(movies)
-  movies.map { |m| m[:director] }
+  movies.map { |m| m[:director].split(' ').last }
         .sort
         .uniq
-        .map { |m| m.split(' ') }
-        .map { |m| m[-1] }
 end
 
 def movies_not_usa(movies)
-  movies.select do |m|
-    unless m[:country].include?('USA')
-      puts m[:name], m[:realise], m[:geners], m[:runtime], m[:stars].chomp!
-    end
-  end
+  movies.reject { |m| m[:country].include?('USA') }
 end
 
-puts movies_not_usa(parse_txt_file(ARGV.first))
+def pretty_print(m)
+  "Title:#{m[:name]}, (#{m[:realise]},#{m[:geners]} - #{m[:runtime]} "
+end
+
+puts parse_txt_file(ARGV.first)
