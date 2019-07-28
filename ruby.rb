da9122b05ee662f2ce0cb508edcc3a1126859cdb
Last_Vegas <AC-130a@yandex.ru>
2019-07-28 15:46:44 +0300

    make new functions
    -make functions with months and years

--- ruby.rb
+++ ruby.rb
@@ -1,5 +1,6 @@
 require 'csv'
 require 'ostruct'
+require 'date'
 
 def parse_txt_file(filename)
   name = filename.nil? ? 'movies.txt' : filename
@@ -17,12 +18,12 @@ def create_movie(movies)
     name: movies[1],
     year: movies[2],
     country: movies[3],
-    realise: movies[4],
+    release: turn_to_date(correct_date(movies[4])).mon,
     geners: movies[5],
     runtime: movies[6],
     rate: rating_to_stars(movies[7].to_f),
     director: movies[8],
-    stars: movies[9]
+    actors: movies[9]
   )
 end
 
@@ -46,7 +47,7 @@ end
 
 def movie_comedy(movies)
   movies.select { |m| m[:geners].include?('Comedy') }
-        .sort_by { |m| m[:realise] }
+        .sort_by { |m| m[:release] }
         .take(10)
 end
 
@@ -61,7 +62,137 @@ def movies_not_usa(movies)
 end
 
 def pretty_print(m)
-  "Title:#{m[:name]}, (#{m[:realise]},#{m[:geners]} - #{m[:runtime]} "
+  "Title:#{m[:name]}, (#{m[:release]},#{m[:geners]} - #{m[:runtime]} "
 end
 
-puts movie_directors_list(parse_txt_file(ARGV.first))
+def turn_to_date(release)
+  Date.parse(release)
+end
+
+def correct_date(date_string)
+  first_day = ('-01')
+  if date_string.length == 7
+    date_string + first_day
+  elsif date_string.length == 4
+    date_string + first_day + first_day
+  elsif date_string.length == 10
+    date_string
+  end
+end
+
+MONS = {
+  1 => 'January',
+  2 => 'February',
+  3 => 'March',
+  4 => 'April',
+  5 => 'May',
+  6 => 'June',
+  7 => 'July',
+  8 => 'August',
+  9 => 'September',
+  10 => 'October',
+  11 => 'November',
+  12 => 'December'
+}.freeze
+
+stats = {
+  'January' => [],
+  'February' => [],
+  'March' => [],
+  'April' => [],
+  'May' => [],
+  'June' => [],
+  'July' => [],
+  'August' => [],
+  'September' => [],
+  'October' => [],
+  'November' => [],
+  'December' => []
+}
+
+stats_year = {
+  '1921' => [],
+  '1925' => [],
+  '1926' => [],
+  '1927' => [],
+  '1931' => [],
+  '1934' => [],
+  '1936' => [],
+  '1939' => [],
+  '1940' => [],
+  '1941' => [],
+  '1942' => [],
+  '1944' => [],
+  '1946' => [],
+  '1948' => [],
+  '1949' => [],
+  '1950' => [],
+  '1951' => [],
+  '1952' => [],
+  '1953' => [],
+  '1954' => [],
+  '1955' => [],
+  '1956' => [],
+  '1957' => [],
+  '1958' => [],
+  '1959' => [],
+  '1960' => [],
+  '1961' => [],
+  '1962' => [],
+  '1963' => [],
+  '1964' => [],
+  '1965' => [],
+  '1966' => [],
+  '1967' => [],
+  '1968' => [],
+  '1969' => [],
+  '1971' => [],
+  '1972' => [],
+  '1973' => [],
+  '1974' => [],
+  '1975' => [],
+  '1976' => [],
+  '1977' => [],
+  '1978' => [],
+  '1979' => [],
+  '1980' => [],
+  '1981' => [],
+  '1982' => [],
+  '1983' => [],
+  '1984' => [],
+  '1985' => [],
+  '1986' => [],
+  '1987' => [],
+  '1988' => [],
+  '1989' => [],
+  '1990' => [],
+  '1991' => [],
+  '1992' => [],
+  '1993' => [],
+  '1994' => [],
+  '1995' => [],
+  '1996' => [],
+  '1997' => [],
+  '1998' => [],
+  '1999' => [],
+  '2000' => [],
+  '2001' => [],
+  '2002' => [],
+  '2003' => [],
+  '2004' => [],
+  '2005' => [],
+  '2006' => [],
+  '2007' => [],
+  '2008' => [],
+  '2009' => [],
+  '2010' => [],
+  '2011' => [],
+  '2012' => [],
+  '2013' => [],
+  '2014' => [],
+  '2015' => []
+}
+
+parse_txt_file(ARGV.first).map { |m| stats_year[m.year] << m }
+
+puts stats_year['2010']
