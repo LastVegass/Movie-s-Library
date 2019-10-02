f42d337f0066579c3d5a146688715ff1814b9a25
Last_Vegas <AC-130a@yandex.ru>
2019-10-02 19:30:03 +0300

    fix issues
    -fix MC class function

--- movie_collection.rb
+++ movie_collection.rb
@@ -26,18 +26,25 @@ class Movie_colletion
     filters.reduce(@collection) { |filtered, (key, value)| filtered.select { |m| m.send(key).include?(value)}  }
   end
 
-  def stats(film)
-    #list_right_films = film.reduce(@collection) { |filtered, (key, value)| filtered.select { |m| m.send(key).include?(value) } }
-    list_main = film.keys.map { |e| @collection.map { |z| z.send(e) } }
-                    .join(',')
-                    .split(',')
-                    .sort
-                    .uniq
-    list_hash = Hash[list_main.collect { |item| [item, []] }]
-    list_hash.keys.map { |key| if film.values.include?(key)
-                                 list_hash[key] << film.reduce(@collection) { |filtered, (key, value)| filtered.select { |m| m.send(key).include?(value) } }
-                               end
-    }
-    list_hash
+  def stats(stats_arguments)
+    right_films = stats_arguments.reduce(@collection) { |filtered, (key, value)| filtered.select { |m| m.send(key).include?(value) } }
+    argument_stats_collection = stats_arguments.keys.map { |stats_key| @collection.map { |movie| movie.send(stats_key)  } }
+                                                                                  .join(',')
+                                                                                  .split(',')
+                                                                                  .sort
+                                                                                  .uniq
+    arguments_hash_list = Hash[argument_stats_collection.collect { |item| [item, []] }]
+    arguments_hash_list.keys.map do |key|
+      if stats_arguments.values.include?(key)
+        arguments_hash_list[key] << right_films
+      end
+    end
+    #stats_arguments.reduce(@collection) { |filtered, (key, value)| filtered.select { |m| m.send(key).include?(value) } }
+    #puts arguments_hash_list.reduce(@collection) { |filter, (key,value)| filter.select { } }
+    #  if stats_arguments.values.include?(key)
+    #    arguments_hash_list[key] << right_films
+    #  end
+    #end
+    arguments_hash_list
   end
 end
